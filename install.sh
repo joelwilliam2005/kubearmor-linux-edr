036f1f3957c1af49f3f3e866b4cc13e757093fae
Joel William <joelwilliam.work@gmail.com>
2026-05-18 14:43:57 +0530

    Update installation script to use INSTALL_DIR variable

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VER=1.7.0
+INSTALL_DIR="/opt/kubearmor-linux-edr"
 
 echo "Installing dependencies..."
 sudo apt install -y curl wget nftables git
@@ -14,13 +15,13 @@ echo "Installing karmor CLI..."
 curl -sfL http://get.kubearmor.io/ | sudo sh -s -- -b /usr/local/bin
 
 echo "Cloning kedr repo..."
-git clone https://github.com/joelwilliam2005/kubearmor-linux-edr ~/kubearmor-linux-edr
+git clone https://github.com/joelwilliam2005/kubearmor-linux-edr $INSTALL_DIR
 
 echo "Installing kedr tools..."
-sudo cp ~/kubearmor-linux-edr/kedr.py /usr/local/bin/kedr.py
-sudo cp ~/kubearmor-linux-edr/kedr /usr/local/bin/kedr
+sudo cp $INSTALL_DIR/kedr.py /usr/local/bin/kedr.py
+sudo cp $INSTALL_DIR/kedr /usr/local/bin/kedr
 sudo chmod +x /usr/local/bin/kedr
 
 echo "Installation Complete."
-echo "Policies are in ~/kubearmor-linux-edr/policies/"
+echo "Policies are in $INSTALL_DIR/policies/"
 echo "Run 'kedr' to start monitoring."
