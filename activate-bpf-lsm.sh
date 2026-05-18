e107a115066e2d55466c16bde811bd3d1de58d0e
Joel William <joelwilliam.work@gmail.com>
2026-05-18 16:03:38 +0530

    Delete activate-bpf-lsm.sh

--- activate-bpf-lsm.sh
+++
@@ -1,5 +0,0 @@
-# Enable BPF LSM
-sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="lsm=lockdown,capability,landlock,yama,apparmor,bpf"/' /etc/default/grub
-sudo update-grub
-
-echo "BPF LSM enabled. Reboot to activate."
